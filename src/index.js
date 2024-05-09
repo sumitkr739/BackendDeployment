c8adb8c2fa7a88c2f2892eafa53f3c44cca76e8f
Sumit Kumar Sinha <sumitsinha739@gmail.com>
2024-05-09 23:31:09 +0530

    connect to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,45 @@
+// import mongoose from "mongoose";
+// import { DB_NAME } from "./utils/constants.js";
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+// require("dotenv").config();
+// const { log } = require("console");
+// const express = require("express");
+// const bodyParser = require("body-parser");
+// const path = require("path");
+// const app = express();
+// const port = 4000;
+
+dotenv.config({
+  path: "./env",
+});
+
+connectDB();
+// app.use(bodyParser.urlencoded());
+
+// (async () => {
+//   try {
+//     await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+//     app.on("error", (error) => {
+//       console.log("ERRR: ", error);
+//       throw error;
+//     });
+
+//     app.listen(process.env.PORT, () => {
+//       console.log(`Server is listing on port ${port}`);
+//     });
+//   } catch (error) {
+//     console.error("ERROR: ", error);
+//     throw error;
+//   }
+// })();
+
+// app.get("/", (req, res) => {
+//   //   res.send("<h1>Hello World!</h1>");
+//   res.sendFile(path.join(__dirname + "/index.html"));
+// });
+
+// app.post("/api/v1/login", (req, res) => {
+//   res.send("Done");
+//   console.log(req.body);
+// });
